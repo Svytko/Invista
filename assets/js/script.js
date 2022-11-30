16765cbc0a7c3e1cd009a9d5dfdb880710a60fe2
Svytko <svutko@gmail.com>
2022-11-30 05:15:31 -0700

    add and correct files

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $('.services__slider').slick({
+//         arrows: false,
+//         slidesToShow: 3,
+//         speed: 1000,
+//         easing:'linear',
+//         infinite:true,
+//         initialSlide:1,
+//         autoplay:true,
+//         autoplaySpeed: 1500,
+//     });
+// });
