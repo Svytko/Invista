6c45fa22dfc1e66016fc2c1a7451cb677ac4ca82
Svytko <svutko@gmail.com>
2022-12-07 09:30:04 -0700

    add section footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,36 @@
 //         autoplaySpeed: 1500,
 //     });
 // });
+
+// let arr = ["xcvx", 1, 23, "5"];
+
+// function typeOfArr(value){
+//     // arr2=[];
+//     // for(let i=0; i<arr.length; i++){
+//     //     if(typeof(arr[i])==='number'){
+//     //         arr2.push(arr[i]);
+//     //     }
+//     //     else{
+//     //         continue;
+//     //     }
+//     // }
+//     // return arr2;
+//     return value.filter(item=>typeof(item)==='number');
+// }
+
+// console.log(typeOfArr(["xcvx", 1, 23, "5"]));
+
+// function sortArr(value){
+//     return value.filter(item=>typeof(item)==="string").map(item=>item.toUpperCase());
+// }
+
+// console.log(sortArr(["xcvx", 1, 23, "5", false]));
+
+
+// 'heelo' = > ['h','e','l','l','o']
+
+function reducer(value){
+    return value.split('').map(value=>value+'!');
+}
+
+console.log(reducer('heelo'));
